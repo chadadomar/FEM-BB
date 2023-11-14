026b51d837d8bc8e1b7cc3cef85c0f14e89d849b
chadadomar <chadadomar@gmail.com>
2023-11-14 18:50:44 +0100

    verification of Bmoments calculations

--- verifications.py
+++ verifications.py
@@ -8,6 +8,8 @@ Created on Wed Dec 29 15:46:56 2021
 from scipy.integrate import quad
 from scipy.integrate import dblquad , tplquad
 from scipy.special import binom
+from Bmoment import *
+import numpy as np
 
 
 
@@ -55,3 +57,34 @@ def integral3D(f,n):
                 l.append(tplquad(integrand3D,0,1,lambda x:0,lambda x:1,lambda x,y:0,lambda x,y:1)[0])
     return l 
 
+
+""" 1d Test 
+def f1d(x):
+    return x*x*np.exp(x)
+
+a=0
+b=1
+n=3
+q=n+1
+
+err1d= abs(integral(a,b,n,f1d)-M1D(a,b,f1d,n,q))
+
+print("erreur 1d integration",err1d) """
+
+""" 2d Test
+
+def f2d(x,y):
+    return x*y*np.exp(x+y)
+
+L2d=[1,0,0,1,0,0]
+err2d= abs(integral2D(f2d,n)-Moment2D(L2d,f2d,n,q))
+print("erreur 2d",err2d) """
+
+
+""" 3d Test 
+def f3d(x,y,z):
+    return x*y*z
+
+L3d=[1,0,0,0,1,0,0,0,1,0,0,0]
+err3d= abs(integral3D(f3d,n)-Moment3D(L3d,f3d,n,q))
+print("erreur 3d",err3d)"""
