20f079bbf2ba8178df0a751cc68903e72f86594d
chadadomar <chadadomar@gmail.com>
2024-02-19 11:51:09 +0100

    third test

--- ASP/Numerical results tables.py
+++ ASP/Numerical results tables.py
@@ -21,7 +21,7 @@ from tabulate import tabulate
 
 sprint = lambda x: '{:.2e}'.format(x)
 
-
+np.seterr('raise')
 
 
 
@@ -31,7 +31,7 @@ sprint = lambda x: '{:.2e}'.format(x)
 
 ps            = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 ks            = (2, 3, 4, 5, 6)
-Taus          = [10**k for k in range(-9,15)]
+Taus          = [10**k for k in range(-14,7)]
 
 # Right hand side and solution
 def f(x,y):
@@ -46,14 +46,29 @@ def curluf(x,y):
 def curlcurluf(x,y):
     return np.array([2,2])
 
+
+
 def uh(x,y):
-    return np.array([np.sin(np.pi*y),np.sin(np.pi*x)]) 
+    return np.array([np.sin(np.pi*y),np.sin(np.pi*x)])
 
 def curlcurluh(x,y):
     return np.array( [  np.pi**2*np.sin(np.pi*y), np.pi**2*np.sin(np.pi*x) ] )
 
 def curluh(x,y):
     return np.pi*np.cos(np.pi*x) - np.pi*np.cos(np.pi*y)
+
+
+def scd(x,y):
+    return np.array([1,1])
+
+def c1(tau):
+    return  -1/(2* tau * np.cosh( np.sqrt(tau)/2 ))
+
+def utau(tau,x,y):
+    return c1(tau) * np.array([ 2* np.cosh( np.sqrt(tau)*(y-0.5) ) , 2* np.cosh( np.sqrt(tau)*(x-0.5) ) ])  + 1/tau * np.array([1,1])
+
+def curlutau(tau,x,y):
+    return 2* (np.sqrt(tau)) * c1(tau) * (np.sinh( np.sqrt(tau)*(x-0.5)) - np.sinh( np.sqrt(tau)*(y-0.5)) )
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                    Creates files which contains results
@@ -65,24 +80,24 @@ def mkdir_p(dir):
     if os.path.isdir(dir):
         return
     os.makedirs(dir)
-    
+
 def create_folder(tau):
-    
+
     tau=str(tau)
-    
+
     folder = 'numerical_results/{tau}'.format(tau=tau)
-            
+
     mkdir_p(os.path.join(folder, 'txt'))
     mkdir_p(os.path.join(folder, 'tex'))
-    
+
     return folder
 
 def write_table(d, folder, kind):
     headers = ['grid/degree p']
-    
+
     for p in ps:
         headers.append(str(p))
-    
+
     # add table rows
     rows = []
     for k in ks:
@@ -95,31 +110,31 @@ def write_table(d, folder, kind):
             elif isinstance(value, int):
                 v = '$'+str(value) +'$'
             else:
-                v =  '$'+sprint(value)+'$' 
+                v =  '$'+sprint(value)+'$'
             row.append(v)
         rows.append(row)
-    
+
     table = tabulate(rows, headers=headers)
-    
-    
-    
-    
+
+
+
+
     fname = '{label}.txt'.format(label=kind)
     fname = os.path.join('txt', fname)
     fname = os.path.join(folder, fname)
-    
+
     with open(fname, 'w', encoding="utf-8") as f:
         table = tabulate(rows, headers=headers, tablefmt ='fancy_grid')
         f.write(str(table))
-    
+
     fname = '{label}.tex'.format(label=kind)
     fname = os.path.join('tex', fname)
     fname = os.path.join(folder, fname)
-    
+
     with open(fname, 'w') as f:
         table = tabulate(rows, headers=headers, tablefmt='latex_raw')
         f.write(str(table))
-        
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                    The main function
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -150,36 +165,41 @@ def main(k, p, tau):
     r=p
     print('============tau = {tau}, p = {p}, k = {k}============'
           .format(tau = tau, p=p,k=k))
-    
+
     mesh_points,mesh_tris,mesh_edges,tris_edges=mesh(k)
     ntris=len(mesh_tris)
     nedges=len(mesh_edges)
-    
-    
+
+
     ndof=nbr_globDof(nedges,ntris,r)
     M=np.zeros((ndof,ndof))
     S=np.zeros((ndof,ndof))
     F=np.zeros(ndof)
-    
-    def rh(x,y):
-        return curlcurluh(x,y)+ tau* uh(x,y)
-    
+
+    #def rh(x,y):
+        #return curlcurluh(x,y)+ tau* uh(x,y)
+
+    def sol_exct(x,y):
+        return utau(tau,x,y)
+    def curl_sol_exct(x,y):
+        return curlutau(tau,x,y)
+
     for i in range(ntris):
         T=mesh_tris[i]
-        
+
         # vertex of the triangle/elem tr
         p0=mesh_points[T[0]]
         p1=mesh_points[T[1]]
         p2=mesh_points[T[2]]
-        
+
         # liste of vertices's coordinates
         Liste=[p0[0],p0[1],p1[0],p1[1], p2[0],p2[1]]
-        
-        # Local element 
+
+        # Local element
         St=Stiff2d(Liste,r)
         Mt=mass2d(Liste,r)
-        Ft=load2d(rh,Liste,r)
-    
+        Ft=load2d(scd,Liste,r)
+
         local_ndof=r*(r+2)
         for j in range(local_ndof):
             glob_j,sign_j=local_to_global(nedges,T, tris_edges[i], i ,j,r)
@@ -188,29 +208,29 @@ def main(k, p, tau):
                 glob_k,sign_k=local_to_global(nedges,T, tris_edges[i], i ,k,r)
                 S[glob_j][glob_k]+= sign_j * sign_k * St[j][k]
                 M[glob_j][glob_k]+= sign_j * sign_k* Mt[j][k]
-    
+
     I=IndexToDelete(mesh_edges,mesh_points,r)
     S=np.delete(S, I,0)
     S=np.delete(S, I,1)
     M=np.delete(M, I,0)
     M=np.delete(M, I,1)
     F=np.delete(F, I,0)
-    
+
     A=S+tau*M
-    
+
     X=np.linalg.solve(A, F)
     newX=reconstruct(X,I)
-    
+
     error_L2=0
     error_Hcurl=0
     error_energy=0
     for i in range(ntris):
         T=mesh_tris[i]
-        
+
         p0=mesh_points[T[0]]
         p1=mesh_points[T[1]]
         p2=mesh_points[T[2]]
-        
+
         Liste=[p0[0],p0[1],p1[0],p1[1],p2[0],p2[1]]
         ndof=r*(r+2)
         Coef=[]
@@ -219,24 +239,24 @@ def main(k, p, tau):
             #print("i j k signe",i,j,k,sign)
             Coef.append(sign*newX[k])
         def L2(x,y):
-            return (Eval_curl(Liste,r,Coef,x,y)[0]-uh(x,y)[0])**2 +   (Eval_curl(Liste,r,Coef,x,y)[1]-uh(x,y)[1])**2
+            return (Eval_curl(Liste,r,Coef,x,y)[0]-sol_exct(x,y)[0])**2 +   (Eval_curl(Liste,r,Coef,x,y)[1]-sol_exct(x,y)[1])**2
         def Hcurl(x,y):
-            return L2(x,y)+  (Eval_curlcurl(Liste,r,Coef,x,y)-curluh(x,y))**2
+            return L2(x,y)+  (Eval_curlcurl(Liste,r,Coef,x,y)-curl_sol_exct(x,y))**2
         def Energy(x,y):
-            return tau*L2(x,y)+  (Eval_curlcurl(Liste,r,Coef,x,y)-curluh(x,y))**2
-        
+            return tau*L2(x,y)+  (Eval_curlcurl(Liste,r,Coef,x,y)-curl_sol_exct(x,y))**2
+
         error_L2+=quad(Liste, L2, r+1)
         error_Hcurl+=quad(Liste, Hcurl, r+1)
         error_energy+=quad(Liste,Energy, r+1)
-        
+
     error_L2=np.sqrt(error_L2)
     error_Hcurl=np.sqrt(error_Hcurl)
     error_energy=np.sqrt(error_energy)
-    
+
     info={'err_l2_norm': error_L2, 'err_Hcurl_norm': error_Hcurl, 'err_energy':error_energy, 'cond_2': np.linalg.cond(A)}
-        
+
     return info
-    
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                             Creates tables
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -246,8 +266,8 @@ def main_tables(tau):
     d = {}
     for p in ps:
         for k in ks:
-            info = main(k      = k, 
-                        p      = p, 
+            info = main(k      = k,
+                        p      = p,
                         tau = tau)
 
             d[p,k] = info
@@ -257,21 +277,21 @@ def main_tables(tau):
     write_table(d, folder, kind ='err_energy')
     write_table(d, folder, kind ='err_Hcurl_norm')
 
-            
-            
-                   
+
+
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                               Run tests
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-if __name__ == '__main__':
+"""if __name__ == '__main__':
     for tau in Taus:
-        main_tables(tau = tau)
-            
+        main_tables(tau = tau)"""
 
-        
-    
-    
-    
+
+
+
+
+
 
 
